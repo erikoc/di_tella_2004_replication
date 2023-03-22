342441836827fa40b9e6a890b10bf77e7a9022cf
erikoc <erik.covarru@gmail.com>
2023-03-22 20:28:22 +0100

    Adjusted testing for convert dtypes, refactored _compute_theft_data into smaller functions for easier testing

--- tests/data_management/test_clean_data.py
+++ tests/data_management/test_clean_data.py
@@ -1,5 +1,7 @@
 import pandas as pd
+import pyreadstat
 import pytest
+from di_tella_2004_replication.config import SRC
 from di_tella_2004_replication.data_management.clean_data import (
     _clean_column_names,
     _convert_dtypes,
@@ -10,6 +12,7 @@ from di_tella_2004_replication.data_management.clean_data import (
 def test_df():
     return pd.DataFrame(
         {
+            "observ": [1, 2, 3],
             "rob1": [1, 0, 1],
             "rob1val": [100, 150, 200],
             "rob2": [1, 1, 0],
@@ -24,12 +27,21 @@ def test_df():
 def expected_df():
     return pd.DataFrame(
         {
-            "theft1": [1, 1, 0],
-            "theft1val": [300, 250, 200],
+            "block": [1, 2, 3],
+            "theft1": [1, 0, 1],
+            "theft1val": [100, 150, 200],
+            "theft2": [1, 1, 0],
+            "theft2val": [300, 250, 200],
             "week_day": ["Monday", "Tuesday", "Wednesday"],
             "census_district": ["A", "B", "C"],
         },
     )
+
+
+@pytest.fixture()
+def original_data_input():
+    data, meta = pyreadstat.read_dta(SRC / "data" / "CrimebyBlock.dta")
+    return data
 
 
 def test_clean_column_names(test_df, expected_df):
@@ -39,11 +51,14 @@ def test_clean_column_names(test_df, expected_df):
 
 def test_clean_column_names_data(test_df, expected_df):
     pd.testing.assert_frame_equal(_clean_column_names(test_df), expected_df)
-    assert isinstance(_clean_column_names(test_df), pd.DataFrame)
 
 
 def test_convert_dtypes(expected_df):
     converted_df = _convert_dtypes(expected_df)
-    for i in range(1, 23):
-        assert isinstance(converted_df[f"theft{i}"], float)
-        assert isinstance(converted_df[f"theft{i}val"], float)
+    theft_cols = [
+        col
+        for col in converted_df.columns
+        if col.startswith("theft") and ("val" in col or col[-1].isdigit())
+    ]
+    for col in theft_cols:
+        assert converted_df[col].dtypes == float
