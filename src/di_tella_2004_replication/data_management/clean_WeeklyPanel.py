4954648758ffa4086d1060182d4b3fb391fee5fa
LuyandoMiau <luyando333@gmail.com>
2023-03-28 16:29:18 +0200

    changes to clean_WeeklyPanel

--- src/di_tella_2004_replication/data_management/clean_WeeklyPanel.py
+++ src/di_tella_2004_replication/data_management/clean_WeeklyPanel.py
@@ -1,4 +1,7 @@
 """Function(s) for cleaning the data set(s)."""
+import pandas as pd
+
+"""Function(s) for cleaning the data set(s)."""
 
 
 def _clean_column_names_we(df):
@@ -36,187 +39,166 @@ def _clean_column_names_we(df):
     return df
 
 
-def _gen_rep_variables_fixedextension_we(
+def _generate_dummy_variables(
     df,
-    var_cond_ext,
-    range_loop=range(1, 40),
-    original_value_var=0,
-    final_value_var=1,
+    variable_dummy
 ):
-    """Generates a set of variables based on a fixed extension, and replaces their
-    values in a Pandas DataFrame.
-
-    Args:
-        df (pandas.DataFrame): The DataFrame in which to generate and replace variables.
-        var_cond_ext (str): The name of the variable in df used to condition the replacement.
-        range_loop (range or iterable): An iterable with the values of the extension to use for variable names.
-            Default is range(1, 40).
-        original_value_var (int or any): The initial value to set to each generated variable.
-            Default is 0.
-        final_value_var (int or any): The final value to set to the variables that match the condition var_cond_ext==i.
-            Default is 1.
-
-    Returns:
-        pandas.DataFrame: The input DataFrame with the generated variables replaced according to the condition.
 
     """
-    for i in range_loop:
-        list_names_ext = []
-        list_names_ext.extend([f"week{i}"])
-        df[list(list_names_ext)] = original_value_var  # generate
-    for i in range_loop:
-        df.loc[df[var_cond_ext] == i, f"week{i}"] = final_value_var  # replace
+    Generate dummy variables from a categorical variable in a Pandas DataFrame.
+
+    Args:
+    df (pandas.DataFrame): The DataFrame containing the variable to be converted into dummy variables.
+    variable (str): The name of the categorical variable to be converted into dummy variables.
+    
+    Returns:
+    pandas.DataFrame: The input DataFrame with an additional column containing the original variable, and with new columns corresponding to the dummy variables.
+
+    """
+
+    df[f"{variable_dummy}_dummy"] = df[variable_dummy]
+    df = pd.get_dummies(df, columns=[f"{variable_dummy}_dummy"], drop_first=False)
     return df
 
 
-def _gen_rep_variables_fixedlistsimple_we(
+
+
+def _generate_variables_from_list(
     df,
     list_fixed,
-    var_cond_fix,
-    var_fix,
-    cond_fix=2,
-    value_var_fix_ori=0,
-    value_var_fix_fin=1,
+    conditional_variable,
+    variable_to_change,
+    conditional_variable_value=2,
+    original_value_list=0,
+    final_value=1
 ):
-    """Generates a set of variables based on a fixed list, and replaces their values in
-    a Pandas DataFrame.
+    """
+    Generates a set of variables based on a fixed list, and replaces their values in a Pandas DataFrame based on a condition.
 
     Args:
-        df (pandas.DataFrame): The DataFrame in which to generate and replace variables.
-        list_fixed (list): A list of column names to generate.
-        var_cond_fix (str): The name of the variable in df used to condition the replacement.
-        var_fix (str): The name of the variable in df to replace its values.
-        cond_fix (int or any): The condition to match in var_cond_fix to apply the replacement.
-            Default is 2.
-        value_var_fix_ori (int or any): The initial value to set to each generated variable.
-            Default is 0.
-        value_var_fix_fin (int or any): The final value to set to the variable var_fix that match the condition.
-            Default is 1.
+    df (pandas.DataFrame): The input DataFrame in which to generate and replace variables.
+    list_fixed (list): A list of column names to generate in the DataFrame.
+    conditional_variable (str): The name of the variable used as a condition to replace values in `variable_to_change`.
+    variable_to_change (str): The name of the variable in `df` whose values are to be replaced based on the condition.
+    conditional_variable_value (int or any, optional): The value of `conditional_variable` that triggers the replacement. Defaults to 2.
+    original_value_list (int or any, optional): The initial value assigned to the generated variables in `list_fixed`. Defaults to 0.
+    final_value (int or any, optional): The value to assign to `variable_to_change` when the `conditional_variable` matches `conditional_variable_value`. Defaults to 1.
 
     Returns:
-        pandas.DataFrame: The input DataFrame with the generated variables replaced according to the condition.
+    pandas.DataFrame: The input DataFrame with the generated variables replaced according to the condition.
 
     """
-    df[list(list_fixed)] = value_var_fix_ori  # generate
-    df.loc[df[var_cond_fix] == cond_fix, var_fix] = value_var_fix_fin  # replace
+    df[list(list_fixed)] = original_value_list  # generate
+    df.loc[df[conditional_variable] == conditional_variable_value, variable_to_change] = final_value  # replace
     return df
 
 
-def _rep_variables_we(
+
+
+def _rep_variables_based_on_condition(
     df,
     type_of_condition,
-    var_cond_rep,
-    replace_var,
-    condition_num=18,
-    value_replace=1,
+    conditional_variable_replace,
+    variable_to_replace,
+    conditional_number=18,
+    final_value_replace=1,
 ):
-    """Generates a set of variables based on a fixed list, and replaces their values in
-    a Pandas DataFrame.
+    """
+    Replaces values of a variable in a Pandas DataFrame based on a condition.
 
     Args:
-        df (pandas.DataFrame): The DataFrame in which to generate and replace variables.
-        list_fixed (list): A list of column names to generate.
-        var_cond_fix (str): The name of the variable in df used to condition the replacement.
-        var_fix (str): The name of the variable in df to replace its values.
-        cond_fix (int or any): The condition to match in var_cond_fix to apply the replacement.
-            Default is 2.
-        value_var_fix_ori (int or any): The initial value to set to each generated variable.
-            Default is 0.
-        value_var_fix_fin (int or any): The final value to set to the variable var_fix that match the condition.
-            Default is 1.
+    df (pandas.DataFrame): The DataFrame in which to replace variables.
+    type_of_condition (str): The type of condition to match. Can be "bigger than", "smaller than", or "equal to".
+    conditional_variable_replace (str): The name of the variable in `df` used as a condition for the replacement.
+    varaible_to_replace (str): The name of the variable in `df` whose values are to be replaced based on the condition.
+    conditional_number (int or any, optional): The value of `conditional_variable_replace` to match with the condition. Defaults to 18.
+    final_value_replace (int or any, optional): The value to replace the values of `variable_to_replace` when the condition is met. Defaults to 1.
 
     Returns:
-        pandas.DataFrame: The input DataFrame with the generated variables replaced according to the condition.
+    pandas.DataFrame: The input DataFrame with the variable values replaced according to the condition.
 
     """
     if type_of_condition == "bigger than":
-        df.loc[df[var_cond_rep] > condition_num, replace_var] = value_replace
+        df.loc[df[conditional_variable_replace] > conditional_number, variable_to_replace] = final_value_replace
         return df
     elif type_of_condition == "smaller than":
-        df.loc[df[var_cond_rep] < condition_num, replace_var] = value_replace
+        df.loc[df[conditional_variable_replace] < conditional_number, variable_to_replace] = final_value_replace
         return df
     elif type_of_condition == "equal to":
-        df.loc[df[var_cond_rep] > condition_num, replace_var] = value_replace
+        df.loc[df[conditional_variable_replace] == conditional_number, variable_to_replace] = final_value_replace
         return df
 
 
-def _gen_rep_variables_fixedlistcomplex_we(
+
+
+def _generate_variables_from_list_complex(
     df,
-    list1_fix_com,
-    list2_fix_com,
-    var_fix_comp_mul,
-    list_rep_fix_com,
-    var_fix_com_to_use="neighborhood",
-    var_fix_com_to_change="n_neighborhood",
-    range_rep_fix_com=range(1, 4),
+    list1,
+    list2,
+    fixed_variable,
+    list_for_replace,
+    fixed_variable_to_use="neighborhood",
+    fixed_variable_to_change="n_neighborhood",
+    range_of_replace=range(1, 4),
 ):
-    """This function generates new variables based on existing variables in a dataframe
-    and replaces the values of a specified variable based on certain conditions.
+    """
+    Replaces values of a variable in a Pandas DataFrame based on a condition.
 
     Args:
-    - df: pandas dataframe
-    - list1_fix_com: list of column names in the dataframe to be multiplied with `var_fix_comp_mul` to generate new variables
-    - list2_fix_com: list of column names in the dataframe to be used in the multiplication
-    - var_fix_comp_mul: variable to be used as a multiplier in the multiplication
-    - list_rep_fix_com: list of values of `var_fix_com_to_use` to be replaced in `var_fix_com_to_change`
-    - var_fix_com_to_use: name of the column in the dataframe to use as condition for replacement (default: 'neighborhood')
-    - var_fix_com_to_change: name of the column in the dataframe to be replaced (default: 'n_neighborhood')
-    - range_rep_fix_com: range of values to use for replacement (default: range(1, 4))
+    df (pandas.DataFrame): The DataFrame in which to replace variables.
+    type_of_condition (str): The type of condition to match. Can be "bigger than", "smaller than", or "equal to".
+    conditional_variable_replace (str): The name of the variable in `df` used as a condition for the replacement.
+    varaible_to_replace (str): The name of the variable in `df` whose values are to be replaced based on the condition.
+    conditional_number (int or any, optional): The value of `conditional_variable_replace` to match with the condition. Defaults to 18.
+    final_value_replace (int or any, optional): The value to replace the values of `variable_to_replace` when the condition is met. Defaults to 1.
 
     Returns:
-    - df: pandas dataframe with new variables generated and specified variable replaced based on conditions
+    pandas.DataFrame: The input DataFrame with the variable values replaced according to the condition.
 
     """
-    for col1, col2 in zip(list1_fix_com, list2_fix_com):
-        df[col1] = df[col2] * df[var_fix_comp_mul]
-    for col, i in zip(list_rep_fix_com, range_rep_fix_com):
-        df.loc[df[var_fix_com_to_use] == col, var_fix_com_to_change] = i
+    for col1, col2 in zip(list1, list2):
+        df[col1] = df[col2] * df[fixed_variable]
+    for col, i in zip(list_for_replace, range_of_replace):
+        df.loc[df[fixed_variable_to_use] == col, fixed_variable_to_change] = i
     return df
 
 
 def weeklypanel(
     df,
-    var_cond_ext,
+    variable_dummy,
     list_fixed,
-    var_cond_fix,
-    var_fix,
+    conditional_variable,
+    variable_to_change,
     type_of_condition,
-    var_cond_rep,
-    replace_var,
-    list1_fix_com,
-    list2_fix_com,
-    var_fix_comp_mul,
-    list_rep_fix_com,
-    location,
-    list_drop=[
-        "street",
-        "street_nr",
-        "public_building_or_embassy",
-        "gas_station",
-        "bank",
-    ],
-    new_var_d="jewish_int_one_block_away_1",
-    var1_d="jewish_inst_one_block_away",
-    var2_d="jewish_inst",
-    new_var_s="code2",
-    var1_s="week",
-    var2_s="n_neighborhood",
-    new_var_sim="n_total_thefts",
-    var_sim="total_thefts",
+    conditional_variable_replace,
+    variable_to_replace,
+    list1,
+    list2,
+    fixed_variable,
+    list_for_replace,
+    fixed_variable_to_use="neighborhood",
+    fixed_variable_to_change="n_neighborhood",
+    range_of_replace=range(1, 4),
+    conditional_number=18,
+    final_value_replace=1,
+    conditional_variable_value=2,
+    original_value_list=0,
+    final_value=1
+
 ):
     df = _clean_column_names_we(df)
     df.drop(columns=list_drop, inplace=True)
-    df = _gen_rep_variables_fixedextension_we(df, var_cond_ext)
-    df = _gen_rep_variables_fixedlistsimple_we(df, list_fixed, var_cond_fix, var_fix)
-    df = _rep_variables_we(df, type_of_condition, var_cond_rep, replace_var)
-    df[new_var_d] = df[var1_d] - df[var2_d]
-    df[new_var_s] = 1 * df[var1_s] + 1000 * df[var2_s]
-    df[new_var_sim] = df[var_sim] * ((365 / 12) / 7)
-    df = _gen_rep_variables_fixedlistcomplex_we(
+    df = _generate_dummy_variables(df, variable_dummy)
+    df = _generate_variables_from_list(df, list_fixed, conditional_variable, variable_to_change)
+    df = _rep_variables_based_on_condition(df, type_of_condition, conditional_variable_replace, variable_to_replace)
+    df[new_variable_diff] = df[var1_d] - df[var2_d]
+    df[new_variable_sum] = 1 * df[var1_s] + 1000 * df[var2_s]
+    df[new_variable_simple] = df[var_sim] * ((365 / 12) / 7)
+    df = _generate_variables_from_list_complex(
         df,
-        list1_fix_com,
-        list2_fix_com,
-        var_fix_comp_mul,
-        list_rep_fix_com,
+        list1,
+        list2,
+        fixed_variable,
+        list_for_replace,
     )
     df.to_csv(location)
