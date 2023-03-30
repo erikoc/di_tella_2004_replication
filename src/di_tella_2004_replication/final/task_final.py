d9fc380df5f7ecedf397d99436e15045f8e57d25
erikoc <erik.covarru@gmail.com>
2023-03-30 15:04:42 +0200

    Created taks for producing latex table for the CrimeByBlock regressions

--- src/di_tella_2004_replication/final/task_final.py
+++ src/di_tella_2004_replication/final/task_final.py
@@ -1,40 +1,86 @@
 """Tasks running the results formatting (tables, figures)."""
+import pickle
 
-"""
+import pytask
 
-from di_tella_2004_replication.analysis.model import load_model
-from di_tella_2004_replication.config import BLD, GROUPS, SRC
-from di_tella_2004_replication.final import plot_regression_by_age
-from di_tella_2004_replication.utilities import read_yaml
-
-for group in GROUPS:
-
-    kwargs = {
-        "group": group,
-        "depends_on": {"predictions": BLD / "python" / "predictions" / f"{group}.csv"},
-        "produces": BLD / "python" / "figures" / f"smoking_by_{group}.png",
-    }
-
-    @pytask.mark.depends_on(
-        {
-            "data_info": SRC / "data_management" / "data_info.yaml",
-            "data": BLD / "python" / "data" / "data_clean.csv",
-        },
-    )
-    @pytask.mark.task(id=group, kwargs=kwargs)
-    def task_plot_results_by_age_python(depends_on, group, produces):
-        data_info = read_yaml(depends_on["data_info"])
-        data = pd.read_csv(depends_on["data"])
-        predictions = pd.read_csv(depends_on["predictions"])
-        fig = plot_regression_by_age(data, data_info, predictions, group)
-        fig.write_image(produces)
+from di_tella_2004_replication.config import BLD
 
 
-@pytask.mark.depends_on(BLD / "python" / "models" / "model.pickle")
-@pytask.mark.produces(BLD / "python" / "tables" / "estimation_results.tex")
-def task_create_results_table_python(depends_on, produces):
-    model = load_model(depends_on)
-    table = model.summary().as_latex()
-    with open(produces, "w") as f:
-        f.writelines(table)
-"""
+@pytask.mark.depends_on(BLD / "python" / "models" / "fe_tot_models.pickle")
+@pytask.mark.produces(
+    {
+        "hv": BLD / "python" / "tables" / "fe_tot_models_results_hv.tex",
+        "lv": BLD / "python" / "tables" / "fe_tot_models_results_lv.tex",
+        "night": BLD / "python" / "tables" / "fe_tot_models_results_night.tex",
+        "day": BLD / "python" / "tables" / "fe_tot_models_results_day.tex",
+        "weekday": BLD / "python" / "tables" / "fe_tot_models_results_weekday.tex",
+        "weekend": BLD / "python" / "tables" / "fe_tot_models_results_weekend.tex",
+    },
+)
+def task_create_results_fe_tot_python(depends_on, produces):
+    with open(depends_on, "rb") as f:
+        model = pickle.load(f)
+    for suffix in ["hv", "lv", "night", "day", "weekday", "weekend"]:
+        table = model[suffix].summary.as_latex()
+        with open(produces[suffix], "w") as f:
+            f.writelines(table)
+
+
+@pytask.mark.depends_on(BLD / "python" / "models" / "abs_tot_models.pickle")
+@pytask.mark.produces(
+    {
+        "hv": BLD / "python" / "tables" / "abs_tot_models_results_hv.tex",
+        "lv": BLD / "python" / "tables" / "abs_tot_models_results_lv.tex",
+        "night": BLD / "python" / "tables" / "abs_tot_models_results_night.tex",
+        "day": BLD / "python" / "tables" / "abs_tot_models_results_day.tex",
+        "weekday": BLD / "python" / "tables" / "abs_tot_models_results_weekday.tex",
+        "weekend": BLD / "python" / "tables" / "abs_tot_models_results_weekend.tex",
+    },
+)
+def task_create_results_abs_tot_python(depends_on, produces):
+    with open(depends_on, "rb") as f:
+        model = pickle.load(f)
+    for suffix in ["hv", "lv", "night", "day", "weekday", "weekend"]:
+        table = model[suffix].summary.as_latex()
+        with open(produces[suffix], "w") as f:
+            f.writelines(table)
+
+
+@pytask.mark.depends_on(BLD / "python" / "models" / "fe_dif_models.pickle")
+@pytask.mark.produces(
+    {
+        "hv_lv": BLD / "python" / "tables" / "fe_dif_models_results_hv_lv.tex",
+        "night_day": BLD / "python" / "tables" / "fe_dif_models_results_night_day.tex",
+        "weekday_weekend": BLD
+        / "python"
+        / "tables"
+        / "fe_dif_models_results_weekday_weekend.tex",
+    },
+)
+def task_create_results_fe_dif_python(depends_on, produces):
+    with open(depends_on, "rb") as f:
+        model = pickle.load(f)
+    for suffix in ["hv_lv", "night_day", "weekday_weekend"]:
+        table = model[suffix].summary.as_latex()
+        with open(produces[suffix], "w") as f:
+            f.writelines(table)
+
+
+@pytask.mark.depends_on(BLD / "python" / "models" / "abs_dif_models.pickle")
+@pytask.mark.produces(
+    {
+        "hv_lv": BLD / "python" / "tables" / "abs_dif_models_results_hv_lv.tex",
+        "night_day": BLD / "python" / "tables" / "abs_dif_models_results_night_day.tex",
+        "weekday_weekend": BLD
+        / "python"
+        / "tables"
+        / "abs_dif_models_results_weekday_weekend.tex",
+    },
+)
+def task_create_results_abs_dif_python(depends_on, produces):
+    with open(depends_on, "rb") as f:
+        model = pickle.load(f)
+    for suffix in ["hv_lv", "night_day", "weekday_weekend"]:
+        table = model[suffix].summary.as_latex()
+        with open(produces[suffix], "w") as f:
+            f.writelines(table)
